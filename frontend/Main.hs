9a24d458110ad22679371a425eb53f627a3b285f
AJ Ruf <z0isch@gmail.com>
2020-04-22 10:43:52 -0400

    state encoding stuff

--- frontend/Main.hs
+++ frontend/Main.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main where
@@ -11,7 +12,12 @@ module Main where
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Aeson
+import qualified Data.ByteString.Lazy as BL
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
+import Data.Map.Strict (Map)
 import Data.Text (Text)
+import Data.Text.Encoding as E
 import Data.Void
 import GHC.Generics (Generic)
 import Language.Javascript.JSaddle
@@ -30,24 +36,58 @@ firebaseConfig =
       appId = "1:1985534572:web:0125b3f478ee74b94ed649"
     }
 
-data R a = R1
+data GameState = WritingQuestions WritingQuestionState | WatingForRound | Round
+  deriving (Show, Generic)
 
-instance Route R Test where
-  renderRoute R1 = "test"
+instance ToJSON GameState
 
-instance Route R (Id, Test) where
-  renderRoute R1 = "test"
+instance FromJSON GameState
 
-data Test = Test {a :: Text, revision :: Revision}
+instance ToJSVal GameState where
+  toJSVal = toJSValAesonText
+
+instance FromJSVal GameState where
+  fromJSVal = fromJSValAesonText
+
+data WritingQuestionState = WritingQuestionState
+  { _writingQuestionStatePlayers :: Map Text (NonEmpty Text)
+  }
+  deriving (Show, Generic)
+
+instance ToJSON WritingQuestionState
+
+instance FromJSON WritingQuestionState
+
+instance ToJSVal WritingQuestionState where
+  toJSVal = toJSValAesonText
+
+instance FromJSVal WritingQuestionState where
+  fromJSVal = fromJSValAesonText
+
+toJSValAesonText :: ToJSON a => a -> JSM JSVal
+toJSValAesonText = toJSVal . E.decodeUtf8 . BL.toStrict . encode
+
+fromJSValAesonText :: FromJSON a => JSVal -> JSM (Maybe a)
+fromJSValAesonText = fmap (join . (fmap (decodeStrict . E.encodeUtf8))) . fromJSVal
+
+data FireBaseState = FireBaseState {state :: GameState, revision :: Revision}
   deriving (Generic, Show)
 
-instance HasRevision Test where
-  getRevision Test {revision} = revision
+instance HasRevision FireBaseState where
+  getRevision FireBaseState {revision} = revision
   setRevision t r = t {revision = r}
 
-instance ToJSVal Test
+instance ToJSVal FireBaseState
 
-instance FromJSVal Test
+instance FromJSVal FireBaseState
+
+data R a = R1
+
+instance Route R FireBaseState where
+  renderRoute R1 = "test"
+
+instance Route R (Id, FireBaseState) where
+  renderRoute R1 = "test"
 
 main :: IO ()
 main =
@@ -58,21 +98,28 @@ main =
       $ \_ -> flip runFirebase firebaseConfig $ do
         pbE <- getPostBuild
         randE <- performEvent $ liftJSM randRevision <$ pbE
-        fbD <- subscribe (Query @R @Test R1 [])
+        fbD <- subscribe (Query @R @FireBaseState R1 [])
+        --add @R @FireBaseState R1 (FireBaseState (WritingQuestions (WritingQuestionState mempty)) <$> randE)
         (transD1, transD2) <-
           (,)
             <$> ( transactionUpdate R1
                     $ ffor randE
                     $ \r ->
-                      ( Id "0Q0TW5fNUmWYWiZJA8jY",
-                        OnlyCurrent $ Test "a" (Revision "3871429086")
+                      ( Id "B3aq3VhaXBqkDvERUzZH",
+                        OnlyCurrent $
+                          FireBaseState
+                            (WritingQuestions (WritingQuestionState ("a" =: (NE.fromList ["oh yeah"]))))
+                            (Revision "2612949843")
                       )
                 )
             <*> ( transactionUpdate R1
                     $ ffor randE
                     $ \r ->
-                      ( Id "0Q0TW5fNUmWYWiZJA8jY",
-                        OnlyCurrent $ Test "b" (Revision "3871429086")
+                      ( Id "B3aq3VhaXBqkDvERUzZH",
+                        OnlyCurrent $
+                          FireBaseState
+                            (WritingQuestions (WritingQuestionState ("b" =: (NE.fromList ["baby"]))))
+                            (Revision "2612949843")
                       )
                 )
         el "pre" $ display transD1
